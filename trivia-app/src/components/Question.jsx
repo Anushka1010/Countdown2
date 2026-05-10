41e636f10120a35cbd7341afeb1ccaaeb792907d
Anushka1010 <136544048+Anushka1010@users.noreply.github.com>
2026-05-10 15:52:24 -0400

    Recreated the Trivia App because of issues with the npm version and updated the button shuffling mechanic so it doesn't happen each time a user clicks an answer and only once in the beginning when the page is loaded.

---
+++ trivia-app/src/components/Question.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import Button from "@mui/material/Button";
+
+function buildAnswerChoices(questionData) {
+    return [
+      {
+        text: questionData.correctAnswer,
+        isCorrect: true,
+      },
+      ...questionData.incorrectAnswers.map((ans) => ({
+        text: ans,
+        isCorrect: false,
+      })),
+    ].sort(() => Math.random() - 0.5);
+  }
+
+const Question = ({ questionData }) => {
+  const [selectedAnswer, setSelectedAnswer] = useState(null);
+  const [answerChoices] = useState(() => buildAnswerChoices(questionData));
+
+  return (
+    <div>
+      <h4>{questionData.question.text}</h4>
+
+      {answerChoices.map((answer, index) => (
+        <Button variant="contained"
+          key={index}
+          onClick={() => setSelectedAnswer(answer)}
+        >
+          {answer.text}
+        </Button>
+      ))}
+
+      {selectedAnswer && (
+        <p>
+          {selectedAnswer.isCorrect ? "Correct!" : "Incorrect :("}
+        </p>
+      )}
+    </div>
+  );
+};
+
+export default Question;
